a1813f802bd5751ba3cbedff1b7e1e2e60942093
coolpositron <coolpositron@gmail.com>
2015-04-30 16:46:24 -0400

    Initial Commit

---
+++ Waymore/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Waymore
+//
+//  Created by Jianhao Li on 4/30/15.
+//  Copyright (c) 2015 Waymore Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Waymore/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Waymore
+//
+//  Created by Jianhao Li on 4/30/15.
+//  Copyright (c) 2015 Waymore Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
