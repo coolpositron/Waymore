2be6bcb5691ff364c0505e7323ff4ae5e67c2929
Yuxuan Wang <yw2666@columbia.edu>
2015-05-10 23:01:49 -0400

    Change ip

--- Waymore/DataAccessManager.m
+++ Waymore/DataAccessManager.m
@@ -29,7 +29,7 @@
 
 - (id) init {
     if (self = [super init]) {
-        self.serverEndPoint = @"http://localhost:8080/JAXRS-Waymore/rest/waymore/";
+        self.serverEndPoint = @"http://160.39.138.223:8080/JAXRS-Waymore/rest/waymore/";
     }
     return self;
 }
